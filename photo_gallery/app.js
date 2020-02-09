b7dc3890d7d983f4789117c9cbc3fab25dfc6a26
marubontan <marubontan@gmail.com>
2020-02-09 17:10:22 -0300

    show images

--- photo_gallery/app.js
+++ photo_gallery/app.js
@@ -1,12 +1,24 @@
 var express = require("express");
+var bodyParser = require("body-parser");
+
 var app = express();
 app.use(express.static("public"));
-app.set("view engine","ejs")
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res)=>{
-    res.render("home.ejs");
+var registeredPhotos = [];
+
+app.get("/", (req, res) => {
+    res.render("home", {registeredPhotos: registeredPhotos});
 })
 
-app.listen(3000, ()=>{
+app.post("/", (req, res) => {
+    registeredPhotos.push(req.body.address);
+    res.redirect("/");
+})
+
+app.listen(3000, () => {
     console.log("App started");
 })
