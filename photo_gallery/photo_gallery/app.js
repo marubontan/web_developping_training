e3fdffe54eb0bc6dd671edbce5a27c67a921a59f
marubontan <marubontan@gmail.com>
2020-02-11 18:38:05 -0300

    add docker compose file

--- photo_gallery/photo_gallery/app.js
+++ photo_gallery/photo_gallery/app.js
@@ -1,5 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:7777")
+
+var ImageSchema = new mongoose.Schema({ address: String });
+var Image = mongoose.model("Image", ImageSchema);
 
 var app = express();
 app.use(express.static("public"));
@@ -8,17 +13,32 @@ app.use(bodyParser.urlencoded({
 }));
 app.set("view engine", "ejs");
 
-var registeredPhotos = [];
 
 app.get("/", (req, res) => {
-    res.render("home", {registeredPhotos: registeredPhotos});
+    Image.find({}, (err, images)=>{
+        if (err) throw err;
+        res.render("home", { registeredPhotos: images });
+    });
 })
 
 app.post("/", (req, res) => {
-    registeredPhotos.push(req.body.address);
+    saveToDB(req.body.address);
     res.redirect("/");
 })
 
 app.listen(3000, () => {
     console.log("App started");
 })
+
+var saveToDB = function(address){
+    var newImage = new Image({ address: address });
+    newImage.save((err, image) => {
+        if (err) {
+            throw err;
+        } else {
+            console.log("Saved address");
+            console.log(image);
+        }
+
+    });
+};
