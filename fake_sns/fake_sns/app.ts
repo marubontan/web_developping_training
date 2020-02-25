bdb694d18fdae9a29771bdc4d0d9cd2c2c3d57b0
Shuhei Kishi <marubontan@gmail.com>
2020-02-24 22:06:52 -0300

    add content prototype

--- fake_sns/fake_sns/app.ts
+++ fake_sns/fake_sns/app.ts
@@ -1,1 +1,14 @@
-console.log("TEST OUTPUT");
+import { Request, Response } from "express";
+import express from "express";
+
+const app = express();
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+app.get("/", (_: Request, res: Response) => {
+    res.render("home");
+})
+
+app.listen(3000, () => {
+    console.log("Service started");
+})
