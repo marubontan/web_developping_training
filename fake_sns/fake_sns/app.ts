fdcb188e93938506f9ca0800ce2aa7767a5c9b78
marubontan <marubontan@gmail.com>
2020-02-25 18:21:29 -0300

    add prototype for content

--- fake_sns/fake_sns/app.ts
+++ fake_sns/fake_sns/app.ts
@@ -1,6 +1,10 @@
 import { Request, Response } from "express";
 import bodyParser from "body-parser";
 import express from "express";
+import mongoose from "mongoose";
+import { postContent } from "./model/user_post"
+
+mongoose.connect("mongodb://mongo:27017:photos", { useNewUrlParser: true });
 
 const app = express();
 app.use(express.static("public"));
@@ -11,13 +15,26 @@ app.use(bodyParser.urlencoded({
 app.set("view engine", "ejs");
 
 app.get("/", (_: Request, res: Response) => {
-    res.render("home", {postContent: ""});
+    postContent.find({}, (err: Error, contents: any) => {
+        if (err) throw err;
+        res.render("home", { postContents: contents });
+
+    })
 })
 
 app.post("/", (req: Request, res: Response) => {
     console.log(req.body.postContent);
-    const postContent: string = req.body.postContent;
-    res.render("home", {postContent: postContent});
+    const content: string = req.body.postContent;
+    const newPost = new postContent({ "content": content });
+    newPost.save((err: Error, _: any) => {
+        if (err) throw err;
+        console.log("post saved");
+    });
+    postContent.find({}, (err: Error, contents: any) => {
+        if (err) throw err;
+        console.log(contents);
+        res.render("home", { postContents: contents });
+    });
 })
 
 app.listen(3000, () => {
