f46532a473632c9e2f7c72dc83b9f11129ea2abc
marubontan <marubontan@gmail.com>
2020-02-25 12:50:36 -0300

    add feed connections

--- fake_sns/fake_sns/app.ts
+++ fake_sns/fake_sns/app.ts
@@ -1,12 +1,23 @@
 import { Request, Response } from "express";
+import bodyParser from "body-parser";
 import express from "express";
 
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
 app.set("view engine", "ejs");
 
 app.get("/", (_: Request, res: Response) => {
-    res.render("home");
+    res.render("home", {postContent: ""});
+})
+
+app.post("/", (req: Request, res: Response) => {
+    console.log(req.body.postContent);
+    const postContent: string = req.body.postContent;
+    res.render("home", {postContent: postContent});
 })
 
 app.listen(3000, () => {
